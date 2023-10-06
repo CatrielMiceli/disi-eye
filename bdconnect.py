4e1a6cbb2961ff50c358a763d2d789d8d7580204
ingscyther <jjarazo@hotmail.com>
2023-10-06 15:45:12 -0300

    emprolijo codigo y actualizo .gitignore

--- bdconnect.py
+++ bdconnect.py
@@ -2,15 +2,9 @@ import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-#def influxdbConnectDISILAB():
-#   token = "TlovmbFuFg-B23L9qhVAJcMZN6o2sUg6Hj5ATCALXZOtc9Rnfrs6pE15ukE7bTSuJ1ebqO1UI0m0zRFBejw0fw=="
-#   org = "sed"
-#   url = "http://localhost:8086"
-#   write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-#   return  write_client
+
 
 def influxdbConnectDISILABFromFile():
-   #client = InfluxDBClient.from_config_file(f'{os.path.dirname(__file__)}/config.ini')
    client = InfluxDBClient.from_config_file("bdconfig.ini")
    return client
 
@@ -22,8 +16,5 @@ def getNumberPerLebelInfluxdb(connect,listObjectDetected, place):
       typeObject=set(listObjectDetected)
       for object in typeObject:
          stringQuery= stringQuery.field(object,(listObjectDetected.count(object)))
-         #print(object+ ': ' + str(listObjectDetected.count(object))+" ",end="")
-      #print(stringQuery)s
       point = (stringQuery)
-      #write_api.write(bucket=bucket, org="sed", record=point)
       write_api.write(bucket=bucket, record=point)  
--- printer.py
+++ printer.py
@@ -7,16 +7,12 @@ def printImageThread(frameBufferProcesed,frameBufferProcesedLock):
     frameBufferProcesedLock.acquire()
     auxFrameBuffer=frameBufferProcesed.copy()
     frameBufferProcesedLock.release()
-    #print(auxFrameBuffer)
     if not(auxFrameBuffer=={}):
       for frame in auxFrameBuffer.keys():
-        #print(frame)
         image=auxFrameBuffer.get(frame)
         image = cv.resize(image,(400,250))
         cv.imshow(frame, image)
         cv.waitKey(1)
-    #auxFrameBuffer={}
-    #time.sleep(0.1)
    cv.destroyAllWindows()
 
 def createPrinter(frameBufferProcesed,frameBufferProcesedLock):
